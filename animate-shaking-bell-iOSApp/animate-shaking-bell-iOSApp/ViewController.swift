0415b8648993c4ac735660892cd17b745292eb30
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-10-11 07:14:52 +1000

    Create third logic part

--- animate-shaking-bell-iOSApp/animate-shaking-bell-iOSApp/ViewController.swift
+++ animate-shaking-bell-iOSApp/animate-shaking-bell-iOSApp/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var bellView: NotificationBadgeView!
     @IBOutlet weak var durationSlider: UISlider!
+    @IBOutlet weak var angleSlider: UISlider!
+    @IBOutlet weak var offsetSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,13 +20,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func durationChanged(_ sender: UISlider) {
-        
         bellView.duration = Double(sender.value)*2
+    }
+    
+    @IBAction func angleChanged(_ sender: UISlider) {
+        let normalized = CGFloat(sender.value) * .pi/2
+        bellView.angle = normalized
+    }
+    
+    @IBAction func offsetChanged(_ sender: UISlider) {
+        bellView.yOffset = CGFloat(sender.value)
     }
     
     @IBAction func reset(_ sender: Any) {
         durationSlider.value = 0.5
-        
+        angleSlider.value = 0.5
+        offsetSlider.value = 0.5
+
         bellView.duration = Double(1)
+        bellView.angle = .pi/8
+        bellView.yOffset = 0.5
     }
 }
